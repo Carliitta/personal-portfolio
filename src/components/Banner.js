5d96f54e10890d1c7c6f1e8c8d0141d6f7253d12
carliitta <carla.rodriguez340@gmail.com>
2023-08-07 20:58:12 -0300

    actualizado

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,26 +18,25 @@ export const Banner = () => {
       <Container>
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+         
+         
                 <span className="tagline">Bienvenid@ a mi Portfolio</span>
                 <h3>{`Hola! soy Carlii`} </h3>
                 <h1>Full Stack Web Developer</h1>
                   <p>Apasionada de la programación y el universo ✨, estoy preparada para abrazar nuevos desafíos y continuar mi crecimiento como profesional.</p>
                   <a className="cv" href={cv} target="_blank" rel="noopener noreferrer" download> Descargar Cv <BiDownload size={25} /></a>
             {/*       <a  href="#Habilidades">Comenzar <ArrowRightCircle size={25} /></a> */}
-              </div>}
-            </TrackVisibility>
+             
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img" className="original-img"/>
-                  <img src={headerImg2} alt="Media Luna" className="media-luna-img"/>
-                </div>}
-            </TrackVisibility>
+          <TrackVisibility>
+          {({ isVisible }) => (
+            <div className={isVisible ? "animate__animated animate__zoomIn" : ""} style={{ width: '300px' }}>
+              <img src={headerImg} alt="Header Img" className="original-img" />
+              <img src={headerImg2} alt="Media Luna" className="media-luna-img" />
+            </div>
+          )}
+        </TrackVisibility>
           </Col>
         </Row>
       </Container>
